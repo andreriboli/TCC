52e87d4552b6525a1db09d30103ecf751d244da5
andre <andre@NOTE56160>
2024-10-10 13:37:16 -0300

    ajuste

--- PainelWebAngular/src/app/app-routing.module.ts
+++ PainelWebAngular/src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { GraficosComponent } from './components/components/graficos/graficos.com
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './components/components/menu/menu.component';
+import { CursosComponent } from './components/components/cursos/cursos.component';
+import { AlunosComponent } from './components/components/alunos/alunos.component';
+import { ProfessoresComponent } from './components/components/professores/professores.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },  // Redireciona para Home por padrão
-  { path: 'home', component: HomeComponent },  // Rota para a Home
-  { path: 'consulta', component: ConsultaComponent },  // Rota para a página de Consultas
-  { path: 'graficos', component: GraficosComponent },  // Rota para a página de Gráficos
-  { path: 'menu', component: MenuComponent },  // Rota para o Menu
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'cursos', component: CursosComponent },
+  { path: 'alunos', component: AlunosComponent },
+  { path: 'professores', component: ProfessoresComponent },
 ];
 
 @NgModule({
